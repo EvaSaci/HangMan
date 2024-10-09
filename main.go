32e0bd7b98fb7004ef408787735d67061939ff1e
damiengmrr <damien.gamarra@ynov.com>
2024-10-09 09:46:08 +0200

    HangMan

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"strings"
 	"time"
+	"unicode"
 )
 
 // Déclaration de difficulte
@@ -20,6 +21,7 @@ const (
 	difficile diff = iota
 	yann      diff = iota
 	raciste   diff = iota
+	h              = iota
 )
 
 func main() {
@@ -31,6 +33,7 @@ func main() {
 	flag.Parse()
 
 	var fichier *os.File
+//	var f *os.File
 	var err error
 
 	// Gestion de la difficulté
@@ -50,6 +53,13 @@ func main() {
 	case "raciste": // mode raciste
 		fmt.Println("Mode raciste")
 		fichier, err = os.Open("mots/motsRaciste.txt") // ouvre le fichier des mots Raciste
+	case "h": // mode help
+		fmt.Println("mode HELP :")
+		fmt.Println("----------------------------------------------------------------")
+		fmt.Println("pour changer de mode : go run . -diff (facile; moyen; difficile)")
+		fmt.Println("vous pouvez annulez le jeu a tous moment avec 'stop'")
+		fmt.Println("----------------------------------------------------------------")
+		fmt.Println("evidemment plusieurs easterEggs sont caché a vous de les trouvez !")
 	default:
 		fmt.Println("Difficulté inconnue. Utilisation de la difficulté moyenne par défaut.") // difficulté par default = moyen
 		fichier, err = os.Open("mots/motsNormal.txt")                                        // ouvre le fichier des mots moyen
@@ -98,16 +108,32 @@ func main() {
 	var test string               // Stocke la lettre ou mot entrée par le joueur
 	estla := make(map[rune]bool)  // Lettres correctes
 	estpas := make(map[rune]bool) // Lettres incorrectes
-
+	VoF := true
 	fmt.Println("Bienvenue dans le jeu Hangman !")
+	fmt.Println("")
+	fmt.Println("plusieurs mode de jeux sont accessible")
+	fmt.Println("--> go run . -h")
+	fmt.Println("")
 	fmt.Println("Prêt ?")
-	fmt.Println("Tu peux annuler en écrivant 'non'")
+	fmt.Println("Tu peux annuler en écrivant 'stop'")
 	fmt.Println("(Si tu veux pas y jouer t'es gay)")
 	fmt.Println("")
 
 	for {
+		cmt := 0
+        cmt2 := 10 - pv + 1
+        lignedebut := (10-pv)*8 + 1
+        lignefin := 9 + lignedebut
+
 		fmt.Print("Entrez une lettre ou un mot: ")
 		fmt.Scan(&test)
+
+		// Vérification si tous les caractères sont valides (lettres et '-')
+		if !valide(test) {
+			fmt.Println("Mauvais caractères, veuillez réessayer") // Message affiché si caractère non valide
+			continue
+		}
+
 		if test == "gay" {
 			fmt.Println("la beuteu a Yann")
 			break
@@ -116,10 +142,14 @@ func main() {
 			fmt.Println("t'es gay")
 			break
 		}
+		if test == "stop" {
+			break
+		}
 
 		// Si le joueur entre un mot entier
 		if len(test) != 1 && test != mot {
 			pv--
+            pv--
 			fmt.Printf("Mot incorrect ! Il vous reste %d chances\n", pv)
 		} else if len(test) != 1 && test == mot {
 			fmt.Printf("Bravo mec ! Tu as trouvé le mot : %s\n", mot)
@@ -131,6 +161,7 @@ func main() {
 			lettre := rune(test[0]) // Convertir la chaîne en rune
 			if strings.ContainsRune(mot, lettre) {
 				estla[lettre] = true
+				VoF = false
 			} else {
 				if !estpas[lettre] {
 					estpas[lettre] = true
@@ -152,17 +183,44 @@ func main() {
 		}
 		fmt.Println()
 
+		// Afficher les lettres incorrectes
+		fmt.Print("Lettres incorrectes : ")
+		for lettre := range estpas {
+			fmt.Printf("%c ", lettre) // jdhgiuerjgoiefrugrdcsfdff
+		}
+		fmt.Println() //
+
 		// Vérifier la victoire
 		if checkWin(mot, estla) {
 			fmt.Printf("Bravo ! Tu as deviné le mot : %s\n", mot)
 			break
 		}
+		f, err := os.Open("O.txt")
+        if err != nil {
+            //log.Fatal(err)
+        }
+		defer f.Close()
+        scanner := bufio.NewScanner(f)
+        for scanner.Scan() {
+            cmt++
 
+            if cmt >= lignedebut && cmt <= lignefin {
+                fmt.Println(scanner.Text())
+            }
+            if cmt%(8*cmt2) == 0 {
+                if VoF {
+                    cmt2++
+                }
+                break
+            }
+        }
+		if err := scanner.Err(); err != nil {
+            //log.Fatal(err)
+        }
 		// Si le joueur perd
 		if pv == 0 {
 			fmt.Println("Vous avez perdu")
 			fmt.Println("Le mot à trouver :", mot) // bite
-			fmt.Printf("   _________\n   ||/     |\n   ||      O\n   ||     /|\\\n   ||     / \\\n   ||\n__/||\\__________\n")
 			break
 		}
 	}
@@ -172,6 +230,16 @@ func main() {
 func checkWin(mot string, estla map[rune]bool) bool {
 	for _, char := range mot {
 		if !estla[char] {
+			return false
+		}
+	}
+	return true
+}
+
+// Fonction pour valider l'entrée (seulement lettres de a à z et le tiret '-')
+func valide(input string) bool {
+	for _, char := range input {
+		if !unicode.IsLetter(char) && char != '-' {
 			return false
 		}
 	}
